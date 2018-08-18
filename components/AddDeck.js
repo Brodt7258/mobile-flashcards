40fd9c84fc7fa677a11c29dd6d67c33fdbc03622
Tim Dill <brodt7258@gmail.com>
2018-08-18 15:29:08 -0400

    fleshed out Add Deck

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
 
 export default class AddDeck extends Component {
   
   render() {
     return (
-      <View>
+      <View style={{padding: 10}}>
         <Text>Add Deck</Text>
+        <TextInput
+          style={{height: 40}}
+          placeholder="What are these cards about?"
+        />
+        <Button
+          title="Submit"
+          onPress={() => console.log("deck added")}
+        />
       </View>
     );
   }
